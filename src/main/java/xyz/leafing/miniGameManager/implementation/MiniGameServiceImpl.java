13bb197e84647b23a3da9801fcc04ae23d85b50e
Roberta001 <2596628651@qq.com>
2025-09-26 20:40:15 +0800

    Initial commit

---
+++ src/main/java/xyz/leafing/miniGameManager/implementation/MiniGameServiceImpl.java
@@ -0,0 +1,4 @@
+package xyz.leafing.miniGameManager.implementation;
+
+public class MiniGameServiceImpl {
+}
---
+++ src/main/java/xyz/leafing/miniGameManager/listeners/PlayerConnectionListener.java
@@ -0,0 +1,4 @@
+package xyz.leafing.miniGameManager.listeners;
+
+public class PlayerConnectionListener {
+}
